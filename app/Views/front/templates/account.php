24632b17b3f7ef2dfb65900c1235b01757a9d2a6
Laurent Guillou <laurent29190@hotmail.fr>
2022-03-24 16:28:06 +0100

    début personnalisation compte

--- app/Views/front/templates/account.php
+++ app/Views/front/templates/account.php
@@ -1,7 +1,7 @@
 <?php include_once ('app/Views/front/layout/header.php') ?>
 
 <main>
-    <h1>Vous êtes connectés !</h1>
+    <h1>Bonjour,  <?= $_SESSION['pseudo'] ?> ! <span class="txt-bzh">Demat deoc'h, <?= $_SESSION['pseudo'] ?> !</span></h1>
 </main>
 
 <?php include_once ('app/Views/front/layout/footer.php') ?>

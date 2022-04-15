b7233d15d45d7bc859db8f6baefada028db7e56a
Laurent Guillou <laurent29190@hotmail.fr>
2022-04-15 15:36:29 +0200

    🐛 FIX: dotenv admin

--- indexAdmin.php
+++ indexAdmin.php
@@ -4,7 +4,7 @@ session_start();
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(DIR);
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
 try {

7b9422f28cec239b76cfb35fa0d105e5711ecac9
Laurent Guillou <laurent29190@hotmail.fr>
2022-06-06 14:59:07 +0200

    Page confirmation retirée

--- app/Views/admin/templates/confirmation.php
+++
@@ -1,8 +0,0 @@
-<?php include_once('app/Views/admin/layout/header.php') ?>
-
-
-            <h1>L'article a bien été publié.</h1>
-        </section>
-    </main>
-
-    <?php include_once('app/Views/admin/layout/footer.php') ?>
